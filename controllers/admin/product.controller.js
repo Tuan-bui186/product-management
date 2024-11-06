0703b6151e98870dc780e8c6616402e7f0d8d645
Tuan Bui <tuanmeo7749@gmail.com>
2024-11-06 15:23:02 +0700

    tinh nang loc theo trang thai

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,14 +2,45 @@ const Product = require("../../models/product.model");
 
 // [Get] /admin/products
 module.exports.product = async (req, res) => {
-  const product = await Product.find({
+  let filter = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+
+  if (req.query.status) {
+    const index = filter.findIndex((item) => item.status == req.query.status);
+    filter[index].class = "active";
+  } else {
+  }
+
+  let find = {
     deleted: false,
-  });
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const product = await Product.find(find);
 
   console.log(product);
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh sach san pham",
     product: product,
+    filter: filter,
   });
 };
