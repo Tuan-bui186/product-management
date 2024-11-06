f3b6024b2b30f06cbfcca28fef0ce9f4f4872491
Tuan Bui <tuanmeo7749@gmail.com>
2024-11-06 14:00:49 +0700

    lay data san pham in ra giao dien

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,15 @@
+const Product = require("../../models/product.model");
+
 // [Get] /admin/products
-module.exports.product = (req, res) => {
+module.exports.product = async (req, res) => {
+  const product = await Product.find({
+    deleted: false,
+  });
+
+  console.log(product);
+
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh sach san pham",
+    product: product,
   });
 };
