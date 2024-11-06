af5e3d318e101cec9de14e1db596070ecf0ae2ac
Tuan Bui <tuanmeo7749@gmail.com>
2024-11-06 10:55:46 +0700

    lam giao dien danh sach san pham

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,7 +1,9 @@
 const dashboardRouter = require("./dashboard.route");
 const systemConfig = require("../../config/system");
+const productRouter = require("./product.route");
 
 const PATH_ADMIN = systemConfig.prefixAmin;
 module.exports = (app) => {
   app.use(PATH_ADMIN + "/dashboard", dashboardRouter);
+  app.use(PATH_ADMIN + "/products", productRouter);
 };
