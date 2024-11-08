ea82fc2aa9adb40716a32b9dd54a4294c6611c90
Tuan Bui <tuanmeo7749@gmail.com>
2024-11-08 20:32:32 +0700

    tinh nang xoa mem

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -71,6 +71,9 @@ module.exports.changeMulti = async (req, res) => {
 // [delete] /admin/product/delete/:id
 module.exports.delete = async (req, res) => {
   const id = req.params.id;
-  await Product.deleteOne({ _id: id });
+  await Product.updateOne(
+    { _id: id },
+    { deleted: true, deletedAt: new Date() }
+  );
   res.redirect("back");
 };
