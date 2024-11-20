bbf39cc29349576fc4726cf306501fb94e8a3c53
Tuan Bui <tuanmeo7749@gmail.com>
2024-11-20 16:16:39 +0700

    day file tinh len cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/product.controller");
 const multer = require("multer");
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+// const storageMulter = require("../../helpers/storageMulter");
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
 const validate = require("../../validates/admin/product.validate");
+
+const upload = multer();
 
 router.get("/", controller.product);
 router.patch("/change-status/:status/:id", controller.changeStatus);
@@ -15,8 +18,10 @@ router.post(
   "/create",
   upload.single("thumbnail"),
   validate.createPost,
+  uploadCloud.upload,
   controller.createPost
 );
+
 router.get("/edit/:id", controller.edit);
 router.patch(
   "/edit/:id",
