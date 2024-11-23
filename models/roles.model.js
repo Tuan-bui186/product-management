b2ead90971d595ed62641c85b999f6d4e507efbb
Tuan Bui <tuanmeo7749@gmail.com>
2024-11-23 20:32:10 +0700

    Tao collection roles

---
+++ models/roles.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-updater");
+mongoose.plugin(slug);
+
+const rolesSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    permissions: {
+      type: Array,
+      default: [],
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Roles = mongoose.model("Roles", rolesSchema, "roles");
+
+module.exports = Roles;
