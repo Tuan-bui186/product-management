2b7dcf7bc37fb88f931f39e213ebf7b22a2b53c9
Tuan Bui <tuanmeo7749@gmail.com>
2024-11-26 16:21:44 +0700

    xay dung trang sua tai khoan

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -46,3 +46,44 @@ module.exports.createPost = async (req, res) => {
     res.redirect(`${systemConfig.prefixAmin}/accounts`);
   }
 };
+
+module.exports.edit = async (req, res) => {
+  const id = req.params.id;
+  let find = {
+    deleted: false,
+    _id: id,
+  };
+  try {
+    const data = await Account.findOne(find);
+
+    const roles = await Roles.find({ deleted: false });
+    res.render("admin/pages/accounts/edit.pug", {
+      pageTitle: "Chỉnh sửa tài khoản",
+      data: data,
+      roles: roles,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAmin}/accounts`);
+  }
+};
+
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  const emailExist = await Account.findOne({
+    email: req.body.email,
+    deleted: false,
+    _id: { $ne: id },
+  });
+  if (emailExist) {
+    req.flash("error", `Email ${req.body.email} đã tồn tại`);
+  } else {
+    if (req.body.password) {
+      req.body.password = md5(req.body.password);
+    } else {
+      delete req.body.password;
+    }
+    await Account.updateOne({ _id: id }, req.body);
+    req.flash("success", "Cập nhật tài khoản thành công");
+  }
+  res.redirect("back");
+};
