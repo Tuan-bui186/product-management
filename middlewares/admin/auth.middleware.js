d33d0bd817b5ab85f03edcd6025ca7e895d86325
Tuan Bui <tuanmeo7749@gmail.com>
2024-11-28 15:48:54 +0700

    authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,14 +1,22 @@
 const { prefixAmin } = require("../../config/system");
 const Account = require("../../models/accounts");
+const Roles = require("../../models/roles.model");
 
 module.exports.requireAuth = async (req, res, next) => {
   if (!req.cookies.token) {
     res.redirect(`${prefixAmin}/auth/login`);
   } else {
-    const users = await Account.findOne({ token: req.cookies.token });
-    if (!users) {
+    const user = await Account.findOne({ token: req.cookies.token }).select(
+      "-password"
+    );
+    if (!user) {
       res.redirect(`${prefixAmin}/auth/login`);
     } else {
+      const role = await Roles.findOne({ _id: user.role_id }).select(
+        "title permissions"
+      );
+      res.locals.user = user;
+      res.locals.role = role;
       next();
     }
   }
