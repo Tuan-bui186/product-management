f5fb42b7673c96eb443e04ce3ca8a71b779c1cc7
Tuan Bui <tuanmeo7749@gmail.com>
2024-12-01 22:08:45 +0700

    Them hien thi san pham noi bat ben client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,16 @@
+const Product = require("../../models/product.model");
+const productHelper = require("../../helpers/product");
+
 // [Get] /
 module.exports.index = async (req, res) => {
+  const productFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active",
+  }).limit(6);
+  const newProductFeatured = productHelper.priceNewProduct(productFeatured);
   res.render("client/pages/home/index.pug", {
     pageTitle: "Trang chủ",
+    productFeatured: newProductFeatured,
   });
 };
