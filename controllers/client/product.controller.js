78903d1a03b6c20a0de0203d790def17f0fba8b1
Tuan Bui <tuanmeo7749@gmail.com>
2024-12-02 23:10:07 +0700

    Trang danh chi tiet san pham ben client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -22,10 +22,21 @@ module.exports.detail = async (req, res) => {
   try {
     const find = {
       deleted: false,
-      slug: req.params.slug,
+      slug: req.params.slugProduct,
       status: "active",
     };
     const product = await Product.findOne(find);
+    if (product.product_category_id) {
+      const category = await ProductCategory.findOne({
+        _id: product.product_category_id,
+        deleted: false,
+        status: "active",
+      });
+      product.category = category;
+    }
+
+    product.priceNew = productHelper.priceNewOneProduct(product);
+
     res.render("client/pages/products/detail.pug", {
       pageTitle: product.title,
       product: product,
--- helpers/product.js
+++ helpers/product.js
@@ -8,3 +8,11 @@ module.exports.priceNewProduct = (products) => {
   });
   return newProducts;
 };
+
+module.exports.priceNewOneProduct = (product) => {
+  const priceNew = (
+    (product.price * (100 - product.discountPercentage)) /
+    100
+  ).toFixed(0);
+  return priceNew;
+};
