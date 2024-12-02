15201661b8cd6339964bf658da9e34125db21363
Tuan Bui <tuanmeo7749@gmail.com>
2024-12-02 21:25:20 +0700

    Trang danh sach san pham ben client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require("../../models/product.model");
 const productHelper = require("../../helpers/product");
+const ProductCategory = require("../../models/products-category.model");
+const productCategoryHelper = require("../../helpers/product-category");
 
 // [Get] /products
 module.exports.index = async (req, res) => {
@@ -30,5 +32,34 @@ module.exports.detail = async (req, res) => {
     });
   } catch (error) {
     res.redirect(`/products`);
+  }
+};
+
+module.exports.category = async (req, res) => {
+  try {
+    const category = await ProductCategory.findOne({
+      slug: req.params.slugCategory,
+      deleted: false,
+      status: "active",
+    });
+
+    const listSubCategory = await productCategoryHelper.getSubCategory(
+      category.id
+    );
+    const listSubCategoryId = listSubCategory.map((item) => item.id);
+
+    const products = await Product.find({
+      product_category_id: { $in: [category.id, ...listSubCategoryId] },
+      deleted: false,
+    }).sort({ position: "desc" });
+
+    const newProducts = productHelper.priceNewProduct(products);
+
+    res.render("client/pages/products/index.pug", {
+      pageTitle: category.title,
+      products: newProducts,
+    });
+  } catch (error) {
+    res.redirect(`/`);
   }
 };
