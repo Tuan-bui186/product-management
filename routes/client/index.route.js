13f7a1b17d81e8c23f80a676bbf6122a61156631
Tuan Bui <tuanmeo7749@gmail.com>
2024-12-07 16:40:43 +0700

    lam giao dien dat hanghang

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,7 @@ const categoryMiddleware = require("../../middlewares/client/category.middleware
 const searchRoute = require("./search.route");
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
 const cartRouter = require("./cart.route");
+const checkoutRouter = require("./checkout.route");
 
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
@@ -12,4 +13,5 @@ module.exports = (app) => {
   app.use("/products", productRouter);
   app.use("/search", searchRoute);
   app.use("/cart", cartRouter);
+  app.use("/checkout", checkoutRouter);
 };
