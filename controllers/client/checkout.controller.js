cf636546f2971bbd68517e16843e18280536f1c2
Tuan Bui <tuanmeo7749@gmail.com>
2024-12-10 15:04:25 +0700

    Lay data gui len databse

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 const Cart = require("../../models/cart.model");
 const productHelper = require("../../helpers/product");
+const Order = require("../../models/order");
 
 module.exports.index = async (req, res) => {
   const cartId = req.cookies.cartId;
@@ -26,4 +27,33 @@ module.exports.index = async (req, res) => {
     pageTitle: "Đặt hàng",
     cartDetail: cart,
   });
+};
+
+module.exports.order = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const userInfo = req.body;
+  const cart = await Cart.findOne({ _id: cartId });
+  let products = [];
+  for (const item of cart.products) {
+    const objectProduct = {
+      product_id: item.product_id,
+      price: 0,
+      discountPercentage: 0,
+      quantity: item.quantity,
+    };
+    const productInfo = await Product.findOne({ _id: item.product_id });
+    objectProduct.price = productInfo.price;
+    objectProduct.discountPercentage = productInfo.discountPercentage;
+    products.push(objectProduct);
+  }
+  const objectOrder = {
+    card_id: cartId,
+    userInfo: userInfo,
+    products: products,
+  };
+  const order = new Order(objectOrder);
+  await order.save();
+
+  await Order.updateOne({ _id: cartId }, { products: [] });
+  res.redirect(`checkout/success/${order.id}`);
 };
